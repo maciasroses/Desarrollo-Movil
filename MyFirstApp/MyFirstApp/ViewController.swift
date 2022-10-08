f9d2e3e459026eb96b53d56b69d5f2aa4cd84709
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2022-10-08 12:42:38 -0500

    Acualizacion de aplicaciones ya con gitignore en el repositorio

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appone
+//
+//  Created by Alumno on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MySecondApp/MySecondApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Macías Romero on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numeroInferior: UITextField!
+    @IBOutlet weak var numeroSuperior: UITextField!
+    var titulo: String = ""
+    
+    @IBAction func division(_ sender: Any) {
+        let dividendoUno = Float (numeroSuperior.text!)
+        let divisorUno = Float ((numeroInferior.text ?? "0"))
+        var mensaje = "Favor de llenar los campos."
+        
+        if (dividendoUno != nil && divisorUno != nil){
+            let resultado = (dividendoUno ?? 0)/(divisorUno ?? 0)
+            mensaje = String(resultado)
+        }
+        //MostrarAlerta(mensaje: mensaje, titulo: "Division");
+        titulo = "Division 2"
+        MostrarAlerta(valor: mensaje)
+    }
+    
+    @IBAction func botonlogin(_ sender: Any) {
+        MostrarAlerta(mensaje: usuario.text!, titulo: "Texto")
+    }
+    
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var usuario: UITextField!
+    @IBOutlet weak var contrasenia: UITextField!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tituloLabel.text = "Segunda Aplicacion"
+        tituloLabel.textColor = UIColor(red:36/255, green: 80/255, blue: 155/255, alpha: 1.0)
+    }
+    
+    func MostrarAlerta(mensaje: String, titulo: String){
+        let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func MostrarAlerta(valor: String){
+        MostrarAlerta(mensaje: valor, titulo: self.titulo)
+    }
+
+}
+
